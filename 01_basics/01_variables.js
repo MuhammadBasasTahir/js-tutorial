1c26ca8a66f19321d0cd10a5135976e56b1db265
MuhammadBasasTahir <basastahirm@gmail.com>
2024-12-30 20:07:50 +0000

    Datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "basas@gmail.com"
+var accountPassword = "12345"
+accountCity = "lahore"
+let accontState;
+// accountId = 2 // not allowed
+// comment shortcut: control slash
+
+accountEmail = "b@gmail.com"
+accountPassword = "313131"
+accountCity = "islamabad"
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accontState])
+
+
+
+/*
+prefer not to use var
+because of issue in block scope and functional scope
+*/
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; // treat all js code as newer version but not needed as v8 engine identifies automatically
+
+//alert( 2 + 2 ) // we are using nodejs not browser
+// console.log(3+3) console.log("hello"); dont do this make it readable semicolon are not necessary
+//there are two js documentations first is tc39 and second is mdn
+
+
+let name = "basas"
+let age = 19
+let isLoggedIn = false
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => no value is assigned
+// symbol=> unique
+
+
+// object
+
+console.log(typeof null); //will show as object and reason for many errors in js
+console.log(typeof undefined); // will show undefined 
+ 
