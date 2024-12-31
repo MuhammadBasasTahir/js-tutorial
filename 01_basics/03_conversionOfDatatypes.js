560c184d2c3157bd6c4b20da76b6b7b3670de830
MuhammadBasasTahir <basastahirm@gmail.com>
2024-12-31 11:20:10 +0000

    comparisons

--- 01_basics/03_conversionOfDatatypes.js
+++ 01_basics/03_conversionOfDatatypes.js
@@ -18,5 +18,74 @@
 // "" => false ; // "basas" => true
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//**************************operations*********************************** */
+let value = 3
+let negValue = -value
+// console.log(negValue)
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2/2)
+// console.log(2**2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " basas"
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1" + 2)
+// console.log(1 + "2")
+// console.log("1" + 2 + 2)
+// console.log(1 + 2 + "2")
+// console.log(+true)
+// console.log(+"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+
+// let gameCounter = 100 let 
+// ++gameCounter;
+// console.log(gameCounter);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
