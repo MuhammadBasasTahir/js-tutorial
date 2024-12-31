b53d60ebe1ed36d9eef7ab01f6ccd0148e567e2d
MuhammadBasasTahir <basastahirm@gmail.com>
2024-12-31 11:47:19 +0000

    datatype summary

--- 01_basics/04_comparisons.js
+++ 01_basics/04_comparisons.js
@@ -13,7 +13,7 @@
 
 
 // ===
-console.log("2" === 2);
+// console.log("2" === 2);
 
 
 
---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,46 @@
+// there are two types of datatypes primitive and nonprimitive 
+// based how data is stored in memory and accessed 
+//primitive
+      // 7 types : String, Number, Boolean, null, undefined, Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId)
+
+const bigNumber = 472984790174927390149485203n
+
+const heros = ["shaktiman", "naagraj", "doga"]
+let myObj = {
+    name: "basas",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world")
+}
+
+console.log(typeof myObj)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Refrence (non primitive)
+      //Array, Object,Functions
