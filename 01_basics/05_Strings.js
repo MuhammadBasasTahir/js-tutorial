33ed1b9ff1edb0fd0914324ba857ab8d2c3ddea7
MuhammadBasasTahir <basastahirm@gmail.com>
2025-01-01 14:32:57 +0000

    string taking very time

--- 01_basics/05_Strings.js
+++ 01_basics/05_Strings.js
@@ -1,16 +1,28 @@
-const name = "hitesh"
-const repoCount = 50
+// const name = "hitesh"
+// const repoCount = 50
 
-// console.log(name + repoCount + "value")
+// // console.log(name + repoCount + "value")
 
-console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+// console.log(`hello my name is ${name} and my repo count is ${repoCount}`);
+// const gameName = new String("basas")
+// console.log(gameName[3])// the string is dealt like an object with many methods copy code in console to see methods
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('b'));
+// const newString = gameName.substring(0,4)
+// console.log(newString)
 
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
 
+const newStringOne = "    basas    "
+console.log(newStringOne);
+console.log(newStringOne.trim());
 
+const url = "https://hitesh.com/hitesh%20choudary"
 
-
-
-
+console.log(url.replace('%20', '_'))
 
 
 
